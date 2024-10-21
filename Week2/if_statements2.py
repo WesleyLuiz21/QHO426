a0cb45091b7b81afd2d0d87e8bf963132ca790b6
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-10-21 19:18:00 +0100

    added fourth task to a new if statement file

---
+++ Week2/if_statements2.py
@@ -0,0 +1,9 @@
+# Fourth Task
+
+print("Please enter a whole number.")
+number = int(input())
+
+if number % 2 == 0:
+    print("The number is an even number.")
+else:
+    print("The number is an odd number.")
