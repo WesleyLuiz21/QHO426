317416bd92351b151968bf839a8f6f729b526b7b
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-10-21 19:04:38 +0100

    added if statements and some code to the week 2 folder

---
+++ Week2/if_statements.py
@@ -0,0 +1,12 @@
+print("What type of book do you like?")
+
+# Taking the user input and storing in a variable called book
+book = input()
+
+if book == "adventure":
+    print("I like adventure books!")
+else:
+    print("Wrong input by user")
+
+print("Finished reading book.")
+
