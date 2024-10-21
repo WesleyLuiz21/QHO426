d381ebf4766aa75030bb49e8cfb35bdf3589134b
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-10-21 18:15:52 +0100

    added other task to the input task file

--- Week1/input_task.py
+++ Week1/input_task.py
@@ -2,3 +2,7 @@ print("Please enter your name")
 name = input()
 print(f"Hello {name}")
 
+print("Please enter your age")
+age = int(input()) # Integer only, not float number.
+print(f"Your age is {age}") # adds age to the console
+
