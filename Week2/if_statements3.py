817ee2303c43827c9c61858126ce09349f9932fe
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-10-21 19:42:50 +0100

    added the fifth exercise to the the if statements 3 file

---
+++ Week2/if_statements3.py
@@ -0,0 +1,21 @@
+# Fifth Task
+
+print("Please enter the first number")
+
+first_number = int(input())
+
+print(first_number)
+
+print("Please enter the second number")
+second_number = int(input())
+
+print(second_number)
+
+if first_number > second_number:
+    print("The second number is the smallest!")
+elif first_number < second_number:
+    print("The first number is the smallest!")
+elif first_number == second_number:
+    print("Both are equal!")
+else:
+    print("Wrong user input by user")
