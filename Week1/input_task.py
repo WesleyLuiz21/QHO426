4a6261f98a972e70a418fa38ca2add55f8f6e05b
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-10-21 18:20:59 +0100

    added variables and inputs as well as some calculations to the input tasks file

--- Week1/input_task.py
+++ Week1/input_task.py
@@ -6,3 +6,13 @@ print("Please enter your age")
 age = int(input()) # Integer only, not float number.
 print(f"Your age is {age}") # adds age to the console
 
+print("please enter your Weight")
+weight = float(input())
+print(f"Your weight is {weight}")
+
+print("please enter your Height")
+height = float(input())
+print(f"Your Height is {height}")
+
+bmi = weight / (height * 2)
+print(f"{name} your BMI is {bmi}")
