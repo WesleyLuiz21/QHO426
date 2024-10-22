9de060e2c021b27a20db5ae15f972563cabcb1e0
Wesley Luiz de Souza <wesley_luiz2001@hotmail.com>
2024-10-22 12:39:26 +0000

    git commit -m

---
+++ Week1/input_task2.py
@@ -0,0 +1,11 @@
+# Task 4
+
+lives = int(input("Please enter the number of live: "))
+energy = int(input("Please enter the energy level: "))
+shield = int(input("Please enter the shield leve: "))
+
+print("\nHealth has been set.\n")
+
+print(f"Lives: {"♥" * lives}")
+print(f"Energy: {"♦" * energy}")
+print(f"Shield: {"♦" * shield}")
