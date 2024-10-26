dfd962c098383ee824e65ecbee22e8e9d3184713
Wesley Souza <wesley_luiz2001@hotmail.com>
2024-10-26 12:40:29 +0100

    added multiple conditions task

---
+++ Week2/multiple_conditions.py
@@ -0,0 +1,10 @@
+type_adventure = str(input("Please enter the type of adventure:"))
+if (type_adventure == "scary") or (type_adventure == "short"):
+    print("Entering the dark forest!")
+
+elif (type_adventure == "safe") or (type_adventure == "long"):
+    print("Taking the safe route!")
+
+else:
+    print("Not sure what route to take.")
+
