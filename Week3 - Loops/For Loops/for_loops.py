0dc6a01451f881554c7baf8a4d87abae43779c0e
Wesley Souza <wesley_luiz2001@hotmail.com>
2024-11-02 12:11:47 +0000

    no problem week 3 for loops

---
+++ Week3 - Loops/For Loops/for_loops.py
@@ -0,0 +1,10 @@
+user_response = int(input("How many mountains should I display?: "))
+print("Displaying...")
+for count in range(0, user_response, 1):
+    print("           __")
+    print("          /  \_  ")
+    print('         /^    \\')
+    print('        /  ^    \_')
+    print("      _/ ^ ^     ^\\")
+    print("     /  ^     ^    \\")
+
