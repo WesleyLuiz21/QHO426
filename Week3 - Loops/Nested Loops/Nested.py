6ab7bb876c67df77dd59ac6ad2a78037d2bd644d
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2024-11-04 20:51:52 +0000

    pqp

---
+++ Week3 - Loops/Nested Loops/Nested.py
@@ -0,0 +1,7 @@
+user_response = int(input("How many rows should a have?"))
+user_response2 = int(input("How many columns should a have?\n"))
+
+for i in range(0, user_response2, 1):
+    for j in range(0, user_response2, 1):
+        print(f"{":-)" * user_response}")
+
