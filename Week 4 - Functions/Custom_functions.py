cf35eb4c77ec7f8f6438daed4523379ec71a8e60
Wesley Luiz de Souza <wesley_luiz2001@hotmail.com>
2024-11-14 14:47:09 +0000

    exercise 2 week 4 done

--- Week 4 - Functions/Custom_functions.py
+++ Week 4 - Functions/Custom_functions.py
@@ -5,3 +5,17 @@ def listen():
     print(f'That was a loud {response}!')
 
 listen()
+
+
+# Exercise 2
+
+def identify():
+    response2 = input('What do you see?')
+    print(response2)
+
+    if response2 == 'a large boulder':
+        print(f'Its time to run!')
+    else:
+        print('We will be fine')
+
+identify()
