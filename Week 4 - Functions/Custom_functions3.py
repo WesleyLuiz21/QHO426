c42bdb649cb0ce0364f5bf7f25195b2158a4a5eb
Wesley Luiz de Souza <wesley_luiz2001@hotmail.com>
2024-11-14 15:04:55 +0000

    exercise 3 week 4 done

---
+++ Week 4 - Functions/Custom_functions3.py
@@ -0,0 +1,14 @@
+
+def escape_by(plan):
+    if plan == 'jumping over':
+        print('We cannot escape that way! The boulder is too big!')
+    elif plan == 'running around':
+        print('We cannot escape that way! The boulder is moving too fast!')
+    elif plan == 'cross bridge ahead':
+        print('that might just work! lets go!')
+    else:
+        print('We cannot escape that way! The boulder is in the way!')
+
+escape_by('jumping over')
+escape_by('running around')
+escape_by('cross bridge ahead')
