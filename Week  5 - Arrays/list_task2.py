e2fb054c4a0666b0e8c1c3d8c7c9883cf69d8b75
Wesley Souza <wesley_luiz2001@hotmail.com>
2024-11-16 12:16:44 +0000

    task 2 week 5 done

---
+++ Week  5 - Arrays/list_task2.py
@@ -0,0 +1,13 @@
+def movements():
+    path = ["Move Forward",10, "Move Backward",5, "Move Left",3, "Move Right",1,]
+    return path
+
+def run_task2():
+    print("Moving...")
+    path = movements()
+
+    for i in range(0, len(path), 2):
+        print(f"{path[i]} for {path[i + 1]} Steps")
+
+if __name__ == "__main__":
+    run_task2()
